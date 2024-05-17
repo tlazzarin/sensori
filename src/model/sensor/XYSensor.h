6698c4506701d4521bb04189a23bd63a2ae1398a
Tommaso Lazzarin <tommaso.lazzarin03@gmail.com>
2024-05-17 16:43:43 +0200

    Unione classi eventsensor e xysensor

--- src/model/sensor/XYSensor.h
+++ src/model/sensor/XYSensor.h
@@ -2,19 +2,17 @@
 #define MODEL_SENSOR_XYSENSOR_H
 #include <QList>
 #include "algorithm"
-#include "AbstractSensor.h"
+#include "EventSensor.h"
 
 namespace model{
     namespace sensor{
-        class XYSensor : public AbstractSensor{
+        class XYSensor : public EventSensor{
             private:
-                QList<int> x;
                 QList<int> y;
             public:
                 explicit XYSensor(QString name); //sensore nuovo
                 explicit XYSensor(QString name,unsigned int id, const QList<int>& xdata, const QList<int>& ydata); //sensore importato dal json
                 void simulate() override;
-                const QList<int>& getX() const;
                 const QList<int>& getY() const;
                 virtual void accept(SensorVisitorInterface& visitor);
         };
